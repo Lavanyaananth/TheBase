4f49a4ea3df1679c1237da99d98bccb0f4c42ca9
lavanyaananth <lavanyaanand.11@gmail.com>
2019-09-29 02:09:44 +0530

    adding function in jsx

--- the-base/src/Person/Person.js
+++ the-base/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p> I'm person component</p>
+    return <p> I'm person component and I'm {Math.floor(Math.random() * 30)} years old</p>
 };
 
 export default person;
