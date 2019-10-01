4341858e93e011cc5d0f39fbcdbcefc752b47a31
lavanyaananth <lavanyaanand.11@gmail.com>
2019-10-02 02:46:51 +0530

    adding state to the class component

--- the-base/src/App.js
+++ the-base/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hi, I'm react app</h1>
-      <p>This is really working ..!</p>
-      <Person name="Joe" age="23"> My hobbies : Cycling, racing</Person>
-      <Person name="Katy" age="32"/>
-      <Person name="Jin" age="12"/>
-      <Person name="Nek" age="26"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name:"Joe" , age:23},
+      {name:"Katy" , age:29},
+      {name:"Jin" , age:32},
+      {name:"Nek" , age:36}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1> Hi, I'm react app</h1>
+        <p>This is really working ..!</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My hobbies : Cycling, racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
